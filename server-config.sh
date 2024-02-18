25fcc705234f22f8c850d6eb737d9d22ac1d0d56
satsdevops <satsdevops@gmail.com>
2024-02-18 01:37:39 +0000

    code

---
+++ server-config.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+sudo yum install git -y
+sudo yum install maven -y
+sudo yum install java-1.8.0-openjdk-devel -y 
+
+if [ -d "addressbook" ]
+then
+   echo "repo is already cloned and exists"
+   cd /home/ec2-user/addressbook
+   git pull origin master
+else
+   git clone https://github.com/preethid/addressbook.git
+   cd addressbook
+   git checkout master
+fi
+
+
+mvn package
